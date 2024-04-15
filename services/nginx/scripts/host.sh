cdc743c9b6c01aaa3c7ca33dd4b4fbf6d4448c29
Jean-Christio Martin <admin@marjc.ch>
2024-04-15 17:05:53 +0200

    feat: better script

--- services/nginx/scripts/host.sh
+++ services/nginx/scripts/host.sh
@@ -43,11 +43,11 @@ done
 if ! grep -q "$host" /etc/hosts; then
     # Return line
     sudo -- sh -c -e "echo '' >> /etc/hosts"
-    sudo -- sh -c -e "echo '127.0.0.1\t${host}' >> /etc/hosts" # IPv4
-    sudo -- sh -c -e "echo '::1\t\t${host}' >> /etc/hosts" # IPv6
-    sudo -- sh -c -e "echo '127.0.0.1\twww.${host}' >> /etc/hosts" # IPv4 www
-    sudo -- sh -c -e "echo '::1\t\twww.${host}' >> /etc/hosts" # IPv6 www
-    ech -e "${GREEN}$host${NC} added to /etc/hosts"
+    sudo -- sh -c -e "echo '127.0.0.1\t${host}.local' >> /etc/hosts" # IPv4
+    sudo -- sh -c -e "echo '::1\t\t${host}.local' >> /etc/hosts" # IPv6
+    sudo -- sh -c -e "echo '127.0.0.1\twww.${host}.local' >> /etc/hosts" # IPv4 www
+    sudo -- sh -c -e "echo '::1\t\twww.${host}.local' >> /etc/hosts" # IPv6 www
+    echo -e "${GREEN}$host${NC} added to /etc/hosts"
 else
     echo -e "${YELLOW}$host${NC} host already existing in /etc/hosts."
 fi
@@ -74,13 +74,13 @@ server {
 }
 EOF
 
-    echo -e "Virtual host for ${GREEN}$host${NC} created and enabled."
+    echo -e "Virtual host for ${GREEN}$host.local${NC} created and enabled."
 else
-    echo -e "Virtual host file for ${YELLOW}$host${NC} already exists."
+    echo -e "Virtual host file for ${YELLOW}$host.local${NC} already exists."
 fi
 
 if [ -f "$NGINX_SITES_CONFIG" ]; then
-    escaped_host=$(echo "$host" | sed 's/\./\\./g')
+    escaped_host=$(echo "$host.local" | sed 's/\./\\./g')
 
     # Prepare the mapping line to insert
     mapping="    '$escaped_host' '$php_version';"
@@ -88,7 +88,7 @@ if [ -f "$NGINX_SITES_CONFIG" ]; then
 
     # Check if the mapping for the host already exists
     if grep -q "$mapping" "$NGINX_SITES_CONFIG"; then
-        echo -e "Mapping for ${YELLOW}$host${NC} already exists in the Nginx configuration."
+        echo -e "Mapping for ${YELLOW}$host.local${NC} already exists in the Nginx configuration."
     else
         echo "Updating Nginx default.conf file..."
         # Prepare the mapping line to insert, ensuring it ends with a newline
@@ -100,6 +100,6 @@ if [ -f "$NGINX_SITES_CONFIG" ]; then
             sudo sed -i -z "s/}\$/$mapping\n}/" "$NGINX_SITES_CONFIG"
         fi
 
-        echo -e "Mapping for ${GREEN}$host${NC} added to the Nginx configuration."
+        echo -e "Mapping for ${GREEN}$host.local${NC} added to the Nginx configuration."
     fi
 fi
